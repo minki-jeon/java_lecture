71e6c92165c970bd1b0c3c95fa1fc3f247bf32bc
minki-jeon <minki.jeon.study@gmail.com>
2025-04-28 17:15:21 +0900

    Book p.75 문제2번 실습

---
+++ src/ch02/lecture/C12VariableName.java
@@ -0,0 +1,20 @@
+package ch02.lecture;
+
+public class C12VariableName {
+    public static void main(String[] args) {
+        // 영문대소문자, $, _, 숫자 포함 가능
+        // 숫자로 시작할 수 없음
+        // 의미있는 이름으로
+        // lowerCamelCase 사용
+        // 예약어(키워드) 사용 불가
+        int modelName;
+//        int class;        // 불가
+//        int 6hour;        // 불가
+        int $value;
+        int _age;
+//        int #name;        // 불가
+//        int int;          // 불가
+
+
+    }
+}
