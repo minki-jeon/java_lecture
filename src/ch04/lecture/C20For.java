6e8c1d5fe94b082dc81af37d95fe10f31660a102
minki-jeon <minki.jeon.study@gmail.com>
2025-05-01 12:33:13 +0900

    중첩반복문 Debugging 실습

---
+++ src/ch04/lecture/C20For.java
@@ -0,0 +1,14 @@
+package ch04.lecture;
+
+public class C20For {
+    public static void main(String[] args) {
+        // debugging
+
+        System.out.println("코드실행 1");
+        System.out.println("코드실행 2");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
