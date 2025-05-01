86060c97f1139a1fa11da70aba7c9e5d650376cf
minki-jeon <minki.jeon.study@gmail.com>
2025-05-01 12:21:31 +0900

    중첩반복문 개인실습

--- src/ch04/practice/C03NestedFor.java
+++ src/ch04/practice/C03NestedFor.java
@@ -92,7 +92,7 @@ public class C03NestedFor {
             System.out.println();
         }
         System.out.println("----------------------");
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < 7; i++) {
             for (int j = 6; j > 1; j--) {
                 if (j < (i + 1)) System.out.print("#");
                 else System.out.print("*");
