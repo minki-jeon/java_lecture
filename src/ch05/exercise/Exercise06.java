8e1cba3d3197ba04436c85ef990a95dde2037fde
minki-jeon <minki.jeon.study@gmail.com>
2025-05-02 16:55:55 +0900

    2차원배열 선언, 초기화

---
+++ src/ch05/exercise/Exercise06.java
@@ -0,0 +1,15 @@
+package ch05.exercise;
+
+public class Exercise06 {
+    //* Reference : [Book. 이것이 자바다] p.201
+    public static void main(String[] args) {
+        int[][] array = {
+                {95, 86},
+                {83, 92, 96},
+                {78, 83, 93, 87, 88}
+        };
+
+        System.out.println(array.length);
+        System.out.println(array[2].length);
+    }
+}
