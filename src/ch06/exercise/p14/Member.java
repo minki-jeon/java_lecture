97a587030ae4f8833b1c5aaa191f38f891b64915
minki-jeon <minki.jeon.study@gmail.com>
2025-05-08 16:00:33 +0900

    Class Member - 파라미터(매개변수)=지역변수 포함 메소드는 this로 필드 명시

---
+++ src/ch06/exercise/p14/Member.java
@@ -0,0 +1,14 @@
+package ch06.exercise.p14;
+
+public class Member {
+    //* Reference : [Book. 이것이 자바다] p.280
+    String name;
+    String id;
+    String password;
+    int age;
+    
+    Member(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
