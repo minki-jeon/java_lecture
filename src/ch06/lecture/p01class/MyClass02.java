1e9f493cbdabdf00231db92f407be967e912fc9d
minki-jeon <minki.jeon.study@gmail.com>
2025-05-08 14:59:55 +0900

    클래스(Class) 생성, 참조

---
+++ src/ch06/lecture/p01class/MyClass02.java
@@ -0,0 +1,10 @@
+package ch06.lecture.p01class;
+
+public class MyClass02 {
+    // 클래스명 규칙 - 영문 대소문자, 숫자, $, _ 조합 (단, 숫자로 시작 불가)
+    // 관습 - UpperCamelCase : 대문자로 시작하며, 각 단어 앞글자만 대문자)
+
+    // 보통 하나의 파일에 하나의 클래스가 작성됨 (클래스명이 곧 파일명)
+
+    int lowerCamelCase;         // 변수명 작성 관습
+}
