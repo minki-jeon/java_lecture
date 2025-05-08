c927f800cb87731e6aa1734d2c11507c901982c2
minki-jeon <minki.jeon.study@gmail.com>
2025-05-08 15:45:59 +0900

    Class Method Overloading

---
+++ src/ch06/sec08/exam04/Calculator.java
@@ -0,0 +1,15 @@
+package ch06.sec08.exam04;
+
+public class Calculator {
+    //* Reference : [Book. 이것이 자바다] p.242
+
+    // 정사각형 넓이
+    double areaRectangle(double width) {
+        return width * width;
+    }
+
+    // 직사각형 넓이
+    double areaRectangle(double width, double height) {
+        return width * height;
+    }
+}
