16d35f5b1a5c4f58afdddc71000870c6f84e1bea
minki-jeon <minki.jeon.study@gmail.com>
2025-05-09 11:14:06 +0900

    final 키워드 변수

--- src/ch06/sec11/exam01/KoreanExample.java
+++ src/ch06/sec11/exam01/KoreanExample.java
@@ -13,7 +13,11 @@ public class KoreanExample {
 //        k1.nation = "USA";
 //        k1.ssn = "123-12-1234";
 
-        k1.name = "김자바";
+        k1.name = "고자바";
+        System.out.println("k1.name = " + k1.name);
+
+        Korean k2 = new Korean("343434-5656565", "hong");
+        System.out.println("k2.ssn = " + k2.ssn);
     }
 
 }
