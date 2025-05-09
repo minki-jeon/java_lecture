bd54c5045e6d98324f611c0b24ad73fc076b6542
minki-jeon <minki.jeon.study@gmail.com>
2025-05-09 09:49:12 +0900

    Class Static키워드-클래스멤버/Static(정적)멤버(필드+메소드)

---
+++ src/ch06/exercise/p17/Printer.java
@@ -0,0 +1,20 @@
+package ch06.exercise.p17;
+
+public class Printer {
+    //* Reference : [Book. 이것이 자바다] p.282
+    public static void println(int value) {
+        System.out.println(value);
+    }
+
+    public static void println(boolean value) {
+        System.out.println(value);
+    }
+
+    public static void println(double value) {
+        System.out.println(value);
+    }
+
+    public static void println(String value) {
+        System.out.println(value);
+    }
+}
