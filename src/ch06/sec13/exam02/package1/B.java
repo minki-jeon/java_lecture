8e6dd69fc10221e42acc33f9bdbd8e6cc3be6d1d
minki-jeon <minki.jeon.study@gmail.com>
2025-05-09 14:48:37 +0900

    접근제한자(access modifier) - 생성자 접근제한자

---
+++ src/ch06/sec13/exam02/package1/B.java
@@ -0,0 +1,8 @@
+package ch06.sec13.exam02.package1;
+
+public class B {
+    //* Reference : [Book. 이것이 자바다] p.267
+    A a1 = new A(true);
+    A a2 = new A(1);
+//    A a3 = new A("문자열");    // private
+}
