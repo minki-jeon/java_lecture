50b2609623fc11643ef6d175037935c9ca849fe2
minki-jeon <minki.jeon.study@gmail.com>
2025-05-12 12:38:05 +0900

    다형성 - 상속관계의 클래스 매개변수

--- src/ch07/sec08/exam02/DriverExample.java
+++ src/ch07/sec08/exam02/DriverExample.java
@@ -1,7 +1,7 @@
 package ch07.sec08.exam02;
 
 public class DriverExample {
-    //* Reference : [Book. 이것이 자바다] p.317
+    //* Reference : [Book. 이것이 자바다] p.322
     public static void main(String[] args) {
         Driver driver = new Driver();
 
