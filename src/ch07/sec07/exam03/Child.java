5b42d0b405c0499d4a8bdb8a74daa50006d17fd5
minki-jeon <minki.jeon.study@gmail.com>
2025-05-12 14:52:26 +0900

    다형성 - 상속관계 간의 클래스와 강제타입변환

---
+++ src/ch07/sec07/exam03/Child.java
@@ -0,0 +1,10 @@
+package ch07.sec07.exam03;
+
+public class Child extends Parent {
+    //* Reference : [Book. 이것이 자바다] p.313
+    public String field2;
+
+    public void method3() {
+        System.out.println("Child-method3()");
+    }
+}
---
+++ src/ch07/sec07/exam03/Parent.java
@@ -0,0 +1,16 @@
+package ch07.sec07.exam03;
+
+public class Parent {
+    //* Reference : [Book. 이것이 자바다] p.313
+
+    public String field1;
+
+    public void method1() {
+        System.out.println("Parent-method1()");
+    }
+
+    public void method2() {
+        System.out.println("Parent-method2()");
+    }
+
+}
