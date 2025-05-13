3152c4f0ff3616172728988a2d4b0242e2db6cc2
minki-jeon <minki.jeon.study@gmail.com>
2025-05-13 14:56:37 +0900

    casting 형변환(자동,강제)

---
+++ src/ch08/lecture/p03cast/App02.java
@@ -0,0 +1,17 @@
+package ch08.lecture.p03cast;
+
+public class App02 {
+    public static void main(String[] args) {
+        MyClass021 a = new MyClass021();
+        MyInterface02 b = a;
+        MyClass021 c = (MyClass021) b;      // 다운캐스팅
+        MyClass022 d = (MyClass022) b;      // ClassCastException
+
+        System.out.println("프로그램 종료");
+    }
+}
+
+// @formatter:off
+interface MyInterface02 {}
+class MyClass021 implements MyInterface02 {}
+class MyClass022 implements MyInterface02 {}
