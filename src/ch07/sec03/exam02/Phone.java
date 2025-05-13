25ada7b71580cd0ee289569e4fc6ca2ced1361ab
minki-jeon <minki.jeon.study@gmail.com>
2025-05-13 10:16:47 +0900

    클래스 super() : 상위 클래스 생성자 호출 (+private 변경)

--- src/ch07/sec03/exam02/Phone.java
+++ src/ch07/sec03/exam02/Phone.java
@@ -2,12 +2,20 @@ package ch07.sec03.exam02;
 
 public class Phone {
     //* Reference : [Book. 이것이 자바다] p.293
-    public String model;
-    public String color;
+    private String model;
+    private String color;
 
     public Phone(String model, String color) {
         this.model = model;
         this.color = color;
         System.out.println("Phone(String model, String color) 생성자 실행");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
     }
 }
--- src/ch07/sec03/exam02/SmartPhoneExample.java
+++ src/ch07/sec03/exam02/SmartPhoneExample.java
@@ -5,7 +5,7 @@ public class SmartPhoneExample {
     public static void main(String[] args) {
         SmartPhone myPhone = new SmartPhone("갤럭시", "은색");
 
-        System.out.println("모델: " + myPhone.model);
-        System.out.println("색상: " + myPhone.color);
+        System.out.println("모델: " + myPhone.getModel());
+        System.out.println("색상: " + myPhone.getColor());
     }
 }
