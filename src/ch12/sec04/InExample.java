38fec3f0d85dac193f1e6ea886b29cef3e68b533
minki-jeon <minki.jeon.study@gmail.com>
2025-05-15 14:35:14 +0900

    JAVA API - System.in

---
+++ src/ch12/sec04/InExample.java
@@ -0,0 +1,36 @@
+package ch12.sec04;
+
+import java.io.IOException;
+
+public class InExample {
+    //* Reference : [Book. 이것이 자바다] p.516
+    public static void main(String[] args) throws IOException {
+        int speed = 0;
+        int keyCode = 0;
+
+        while (true) {
+            // Enter 키를 읽지 않았을 경우에만 실행
+            if (keyCode != 13 && keyCode != 10) {
+                if (keyCode == 49) {
+                    // 숫자 1 키
+                    speed++;
+                } else if (keyCode == 50) {
+                    // 숫자 2 키
+                    speed--;
+                } else if (keyCode == 51) {
+                    // 숫자 3 키
+                    break;
+                }
+                System.out.println("--------------------------");
+                System.out.println("1. 증속 | 2. 감속 | 3. 중지");
+                System.out.println("--------------------------");
+                System.out.println("현재 속도 = " + speed);
+                System.out.println("선택: ");
+            }
+
+            // 키를 하나씩 읽음 (int)
+            keyCode = System.in.read();
+        }
+        System.out.println("프로그램 종료");
+    }
+}
