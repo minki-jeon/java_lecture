6ef737652a33c7b55fb0b1d3bdd421333e79694f
minki-jeon <minki.jeon.study@gmail.com>
2025-05-16 12:15:04 +0900

    참조타입(WrapperClass) - Boxing, Unboxing

--- src/ch12/sec06/ValueCompareExample.java
+++ src/ch12/sec06/ValueCompareExample.java
@@ -6,15 +6,20 @@ public class ValueCompareExample {
         // -128~127 초과값일 경우
         Integer obj1 = 300;
         Integer obj2 = 300;
-        System.out.println("==: " + (obj1 == obj2));
-        System.out.println("equals(): " + obj1.equals(obj2));
+        System.out.println("==: " + (obj1 == obj2));            // false
+        System.out.println("equals(): " + obj1.equals(obj2));   // true
+        System.out.println();
+
+        int a = obj1;
+        int b = obj2;
+        System.out.println(a == b);     // true
         System.out.println();
 
         // -128~127 범위값일 경우
         Integer obj3 = 10;
         Integer obj4 = 10;
-        System.out.println("==: " + (obj3 == obj4));
-        System.out.println("equals(): " + obj3.equals(obj4));
+        System.out.println("==: " + (obj3 == obj4));        // true
+        System.out.println("equals(): " + obj3.equals(obj4));   // true
 
     }
 }
