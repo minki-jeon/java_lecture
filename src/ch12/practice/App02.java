388b697bc6b228d307a60b73c1660ad41f3b78a9
minki-jeon <minki.jeon.study@gmail.com>
2025-05-16 10:16:38 +0900

    정규표현식(regex;Regular Expression)

--- src/ch12/practice/App02.java
+++ src/ch12/practice/App02.java
@@ -8,19 +8,9 @@ public class App02 {
         // 사용자로부터 입력받아
         // 한글 3글자 이상인지 확인하는 코드 작성
         Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
         String regex = "[가-힣]{3,}";
-
-        while (true) {
-            System.out.print("입력> ");
-            String str = sc.nextLine();
-
-            if (Pattern.matches(regex, str)) {
-                System.out.println("패턴 일치");
-            } else {
-                System.out.println("패턴 불일치");
-            }
-                
-            System.out.println();
-        }
+        System.out.println(Pattern.matches(regex, str));
+        
     }
 }
