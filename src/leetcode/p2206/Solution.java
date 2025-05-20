5039e3c7902c43d4eb903fa1e52f0c3fa17afa0c
minki-jeon <minki.jeon.study@gmail.com>
2025-05-20 14:50:44 +0900

    2206. Divide Array Into Equal Pairs // map, entrySet

---
+++ src/leetcode/p2206/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p2206;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public boolean divideArray(int[] nums) {
+        // 2206. Divide Array Into Equal Pairs
+        boolean result = true;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            if (map.containsKey(n)) {
+                map.put(n, map.get(n) + 1);
+            } else {
+                map.put(n, 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() % 2 != 0) result = false;
+        }
+
+        return result;
+    }
+}
