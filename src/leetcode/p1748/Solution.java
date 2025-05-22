d029b090b8d3318b79aa50ba290df5fbbac0e884
minki-jeon <minki.jeon.study@gmail.com>
2025-05-22 14:37:31 +0900

    1748. Sum of Unique Elements // stream

--- src/leetcode/p1748/Solution.java
+++ src/leetcode/p1748/Solution.java
@@ -1,12 +1,26 @@
 package leetcode.p1748;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Solution {
     public int sumOfUnique(int[] nums) {
         // 1748. Sum of Unique Elements
 
+        // Stream
+        Map<Integer, Long> map = Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        return map.entrySet().stream()
+                .filter(e -> e.getValue() == 1)
+                .map(e -> e.getKey())
+                .mapToInt(Integer::intValue)
+                .sum();
+
+/*
         // nums 전체 탐색해서 각 아이템이 몇 번 나오는지 카운트하기
         Map<Integer, Integer> map = new HashMap<>();
         for (int n : nums) {
@@ -28,5 +42,7 @@ public class Solution {
         }
 
         return sum;
+ */
+
     }
 }
