422735b0b3ca2135c7ce7fdadc440f8b7b2646cb
minki-jeon <minki.jeon.study@gmail.com>
2025-05-22 14:16:25 +0900

    2206. Divide Array Into Equal Pairs // stream

--- src/leetcode/p2206/Solution.java
+++ src/leetcode/p2206/Solution.java
@@ -1,12 +1,37 @@
 package leetcode.p2206;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class Solution {
+    // 2206. Divide Array Into Equal Pairs
     public boolean divideArray(int[] nums) {
-        // 2206. Divide Array Into Equal Pairs
+        // stream
 
+//        Map<Integer, Long> map = Arrays.stream(nums)
+//                .boxed()
+//                .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+//        return map.values().stream().allMatch(e -> e % 2 == 0);
+
+//        Map<Integer, Long> map = Arrays.stream(nums)
+//                .boxed()
+////                .collect(Collectors.groupingBy(Function.identity(), counting()));
+//                .collect(groupingBy(identity(), counting()));
+//        return map.values().stream().allMatch(e -> e % 2 == 0);
+        
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(groupingBy(identity(), counting()))
+                .values().stream().allMatch(e -> e % 2 == 0);
+
+/*
         Map<Integer, Integer> map = new HashMap<>();
         for (int n : nums) {
             if (map.containsKey(n)) {
@@ -29,5 +54,6 @@ public class Solution {
         // }
 
         return true;
+*/
     }
 }
