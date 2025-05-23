4c5815aacad1eda1e578ae943670946d11a72c2e
minki-jeon <minki.jeon.study@gmail.com>
2025-05-23 14:24:13 +0900

    Thread 객체 생성, 사용 + beep

--- src/ch14/sec03/exam02/BeepPrintExample.java
+++ src/ch14/sec03/exam02/BeepPrintExample.java
@@ -5,16 +5,13 @@ import java.awt.*;
 public class BeepPrintExample {
     //* Reference : [Book. 이것이 자바다] p.599
     public static void main(String[] args) {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Toolkit toolkit = Toolkit.getDefaultToolkit();
-                for (int i = 0; i < 5; i++) {
-                    toolkit.beep();
-                    try {
-                        Thread.sleep(500);
-                    } catch (Exception e) {
-                    }
+        Thread thread = new Thread(() -> {
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            for (int i = 0; i < 5; i++) {
+                toolkit.beep();
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
                 }
             }
         });
