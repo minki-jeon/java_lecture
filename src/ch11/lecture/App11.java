e28f155388d83cad906516a7b269d708c6a3ed42
minki-jeon <minki.jeon.study@gmail.com>
2025-05-23 10:39:22 +0900

    Exception - checked exception(일반예외)

---
+++ src/ch11/lecture/App11.java
@@ -0,0 +1,13 @@
+package ch11.lecture;
+
+public class App11 {
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("java.lang.String3");
+            System.out.println("코드.");
+        } catch (ClassNotFoundException e) {    // checked-exception
+            e.printStackTrace();
+        }
+    }
+}
