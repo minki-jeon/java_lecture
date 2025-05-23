26a72be4d0f40437fb142e23ef361ef76feb0fb4
minki-jeon <minki.jeon.study@gmail.com>
2025-05-23 14:21:21 +0900

    Thread 객체 생성, 사용 + beep

---
+++ src/ch14/lecture/App02.java
@@ -0,0 +1,27 @@
+package ch14.lecture;
+
+import java.awt.*;
+
+public class App02 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            for (int i = 0; i < 5; i++) {
+                // 비프음 발생
+                toolkit.beep();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread.start();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띠리링");
+
+            Thread.sleep(1000);
+        }
+    }
+}
---
+++ src/ch14/sec03/exam02/BeepPrintExample.java
@@ -0,0 +1,33 @@
+package ch14.sec03.exam02;
+
+import java.awt.*;
+
+public class BeepPrintExample {
+    //* Reference : [Book. 이것이 자바다] p.599
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                for (int i = 0; i < 5; i++) {
+                    toolkit.beep();
+                    try {
+                        Thread.sleep(500);
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        });
+
+        thread.start();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+
+        }
+    }
+}
