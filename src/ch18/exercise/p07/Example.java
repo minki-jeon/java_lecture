50639ac980c877205d1dc95f2c51b8ab5abb388f
minki-jeon <minki.jeon.study@gmail.com>
2025-05-26 15:14:14 +0900

    확인문제 BufferedReader

--- src/ch18/exercise/p07/Example.java
+++ src/ch18/exercise/p07/Example.java
@@ -12,17 +12,19 @@ public class Example {
         FileReader fr = new FileReader(filePath);
         BufferedReader br = new BufferedReader(fr);
 
-        int rowNumber = 0;
-        String rowData;
-        while (true) {
-            rowNumber++;
-            rowData = br.readLine();
-            if (rowData == null) break;
-            System.out.println(rowNumber + "\t" + rowData);
+        try (fr; br) {
+            int rowNumber = 0;
+            String rowData;
+            while (true) {
+                rowNumber++;
+                rowData = br.readLine();
+                if (rowData == null) break;
+                System.out.println(rowNumber + "\t" + rowData);
+            }
+//            br.close();
+//            fr.close();
         }
 
-        br.close();
-        fr.close();
 
     }
 }
