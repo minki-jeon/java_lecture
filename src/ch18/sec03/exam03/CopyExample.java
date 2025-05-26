1a3ba107cd680dab705b3b841d9c9d1a552911de
minki-jeon <minki.jeon.study@gmail.com>
2025-05-26 11:45:34 +0900

    CopyFile

--- src/ch18/sec03/exam03/CopyExample.java
+++ src/ch18/sec03/exam03/CopyExample.java
@@ -10,16 +10,18 @@ public class CopyExample {
 
         InputStream is = new FileInputStream(originalFileName);
         OutputStream os = new FileOutputStream(targetFileName);
+        try (is; os;) {
 
-        byte[] data = new byte[1024];
-        while (true) {
-            int num = is.read(data);
-            if (num == -1) break;
-            os.write(data, 0, num);
+            byte[] data = new byte[1024];
+            while (true) {
+                int num = is.read(data);
+                if (num == -1) break;
+                os.write(data, 0, num);
+            }
+            os.flush();
+//            os.close();
+//            is.close();
         }
-        os.flush();
-        os.close();
-        is.close();
 
         System.out.println("복사가 잘 되었습니다.");
     }
