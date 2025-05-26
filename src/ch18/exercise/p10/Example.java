d4bf56cc0c2c61eea97ca2aa3924396459dd0b2b
minki-jeon <minki.jeon.study@gmail.com>
2025-05-26 15:56:39 +0900

    확인문제 보조스트림 활용 FileCopy

---
+++ src/ch18/exercise/p10/Example.java
@@ -0,0 +1,42 @@
+package ch18.exercise.p10;
+
+import java.io.*;
+
+public class Example {
+    //* Reference : [Book. 이것이 자바다] p.824
+    public static void main(String[] args) throws IOException {
+        String origin = "images/eagle.jpg";
+        String target = "D:\\01.private_work\\Choongang\\workspaces\\Temp\\eagle.jpg";
+
+        // 파일 복사
+        // BufferedInputStream, BufferedOutputStream
+
+        System.out.println("원본 파일 경로: " + origin);
+        System.out.println("복사 파일 경로: " + target);
+
+        try {
+            InputStream is = new FileInputStream(origin);
+            BufferedInputStream bis = new BufferedInputStream(is);
+
+            OutputStream os = new FileOutputStream(target);
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+
+            try (is; bis; os; bos) {
+                byte[] data = new byte[1024];
+                int length = 0;
+                while ((length = is.read(data)) != -1) {
+                    bos.write(data, 0, length);
+                }
+                bos.flush();
+            }
+
+            System.out.println("복사가 성공되었습니다.");
+
+        } catch (IOException e) {
+//            e.printStackTrace();
+            System.out.println("원본 파일이 존재하지 않습니다.");
+        }
+
+
+    }
+}
