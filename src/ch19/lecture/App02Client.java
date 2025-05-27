f510cfdbf928e6c1e07d3db6dad6a822daa371f4
minki-jeon <minki.jeon.study@gmail.com>
2025-05-27 14:27:35 +0900

    Server-Client 이미지 파일 송수신

---
+++ src/ch19/lecture/App02Client.java
@@ -0,0 +1,32 @@
+package ch19.lecture;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class App02Client {
+    public static void main(String[] args) throws Exception {
+        String file = "D:\\01.private_work\\Choongang\\workspaces\\Temp\\download.jpg";
+
+        // client: server 연결 시도
+        Socket socket = new Socket("192.168.0.28", 9876);
+        try (socket) {
+            InputStream is = socket.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            // InputStream으로 파일 수신 (Write)
+            FileOutputStream fos = new FileOutputStream(file);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            try (bos; fos; bis; is) {
+                byte[] data = new byte[1024];
+                int len = 0;
+
+                while ((len = bis.read(data)) != -1) {
+                    bos.write(data, 0, len);
+                }
+                bos.flush();
+            }
+        }
+    }
+}
