63f3aecccf51208438425ed88a0207f2fa3e0358
minki-jeon <minki.jeon.study@gmail.com>
2025-05-27 15:17:04 +0900

    Server-Client 다중Client 메시지 수신 + client콘솔입력

--- src/ch19/lecture/App04Client.java
+++ src/ch19/lecture/App04Client.java
@@ -5,19 +5,27 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class App04Client {
     public static void main(String[] args) {
-//        try (Socket socket = new Socket("192.168.0.5", 9876)) {
-        try (Socket socket = new Socket("192.168.0.28", 9876)) {
+        try (Socket socket = new Socket("192.168.0.5", 9876)) {
+//        try (Socket socket = new Socket("192.168.0.28", 9876)) {
             OutputStream os = socket.getOutputStream();
             OutputStreamWriter osw = new OutputStreamWriter(os);
             BufferedWriter bw = new BufferedWriter(osw);
             PrintWriter pw = new PrintWriter(bw);
 
+            Scanner sc = new Scanner(System.in);
+
             try (pw; bw; osw; os) {
-                pw.println("안녕하세요. 저는 Client 입니다.");
-                pw.flush();
+//                pw.println("안녕하세요. 저는 Client 입니다.");
+//                pw.flush();
+                while (true) {
+                    System.out.print("입력>> ");
+                    pw.println(sc.nextLine());
+                    pw.flush();
+                }
             }
         } catch (Exception e) {
         }
